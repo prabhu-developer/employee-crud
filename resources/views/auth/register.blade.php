20359a549736c239c0526308944af2d7618cac8d
prabhu-devloper <prabhu.k.code@gmail.com>
2023-08-13 12:47:53 +0530

    FIX : Auth Route & Code correction

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,6 +66,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
+                                <a class="btn btn-link" href="{{ route('login') }}">
+                                    Already have an account?
+                                </a>
                             </div>
                         </div>
                     </form>
